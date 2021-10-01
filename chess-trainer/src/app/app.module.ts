5bd1355449f11c6d39996fe9de3acd4d99943008
Carles Tutusaus <ctutum@protonmail.com>
2021-10-01 20:19:18 +0200

    Angular material added and screen distribution of the edition board

--- chess-trainer/src/app/app.module.ts
+++ chess-trainer/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './common/component/board/board.component';
 import { EditionBoardComponent } from './edition-board/edition-board.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [AppComponent, BoardComponent, EditionBoardComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxChessBoardModule.forRoot()],
+  imports: [BrowserModule, AppRoutingModule, NgxChessBoardModule.forRoot(), MatCardModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
