7a460bb80ca752de90db92cbde3229491509e3d8
Carles Tutusaus <ctutum@protonmail.com>
2021-10-23 21:03:46 +0200

    Refactor move pipe

--- chess-trainer/src/app/common/pipe/move.pipe.ts
+++ chess-trainer/src/app/common/pipe/move.pipe.ts
@@ -6,10 +6,36 @@ import { MoveEvent } from '../model/interface/move-event';
 })
 export class MovePipe implements PipeTransform {
   transform(moveEvent: MoveEvent): string {
-    //TODO refactor + case two pieces can go  same square
+    //TODO case two pieces can go  same square
     const endSquare = moveEvent.move.slice(-2);
     let moveNumber = moveEvent.color === 'white' ? moveEvent.fen.slice(-1) : parseInt(moveEvent.fen.slice(-1)) - 1;
     const dots = moveEvent.color === 'white' ? '.' : '...';
+    let pieceLetter = this.selectPieceLetter(moveEvent);
+    const captureSimbol = moveEvent.capture ? 'x' : '';
+    const endDecorator = moveEvent.checkmate ? '#' : moveEvent.check ? '+' : '';
+    let moveToShow = this.selectMoveToShow(pieceLetter, captureSimbol, endSquare, endDecorator, moveEvent);
+    return moveNumber + dots + moveToShow;
+  }
+
+  private selectMoveToShow(
+    pieceLetter: string,
+    captureSimbol: string,
+    endSquare: string,
+    endDecorator: string,
+    moveEvent: MoveEvent
+  ) {
+    // Normal move
+    let moveToShow = pieceLetter + captureSimbol + endSquare + endDecorator;
+    // Castle move
+    if (moveEvent.piece === 'King' && (moveEvent.move === 'e1g1' || moveEvent.move === 'e8g8')) {
+      moveToShow = 'O-O';
+    } else if (moveEvent.piece === 'King' && (moveEvent.move === 'e1c1' || moveEvent.move === 'e8c8')) {
+      moveToShow = 'O-O-O';
+    }
+    return moveToShow;
+  }
+
+  private selectPieceLetter(moveEvent: MoveEvent) {
     let pieceLetter = '';
     if (moveEvent.piece !== 'Pawn') {
       if (moveEvent.piece === 'Knight') {
@@ -20,14 +46,6 @@ export class MovePipe implements PipeTransform {
     } else if (moveEvent.capture) {
       pieceLetter = moveEvent.move[0];
     }
-    const captureSimbol = moveEvent.capture ? 'x' : '';
-    const endDecorator = moveEvent.checkmate ? '#' : moveEvent.check ? '+' : '';
-    let moveToShow = pieceLetter + captureSimbol + endSquare + endDecorator;
-    if (moveEvent.piece === 'King' && (moveEvent.move === 'e1g1' || moveEvent.move === 'e8g8')) {
-      moveToShow = 'O-O';
-    } else if (moveEvent.piece === 'King' && (moveEvent.move === 'e1c1' || moveEvent.move === 'e8c8')) {
-      moveToShow = 'O-O-O';
-    }
-    return moveNumber + dots + moveToShow;
+    return pieceLetter;
   }
 }
