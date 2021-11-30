d0a125c3953a66eda7a1f2149a4f39f4027616b4
Carles Tutusaus <ctutum@protonmail.com>
2021-11-30 20:13:24 +0100

    Moved host to environement variables

--- chess-trainer/src/app/common/utils/http-utils.ts
+++ chess-trainer/src/app/common/utils/http-utils.ts
@@ -1,24 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { DeleteMoveBody } from '../model/interface/delete-move-body';
-import { Position } from '../model/interface/position';
 import { PostMoveBody } from '../model/interface/post-move-body';
 
 export class HttpUtils {
   static getPosition(lineUuid: string, fenPosition: string, http: HttpClient): Observable<Object> {
-    return http.request('GET', `http://localhost:8080/chess-trainer/position/${lineUuid}/${fenPosition}`, {
+    return http.request('GET', `${environment.apiUrl}/position/${lineUuid}/${fenPosition}`, {
       responseType: 'json',
     });
   }
 
   static postMoveEvent(body: PostMoveBody, http: HttpClient): Observable<Object> {
-    return http.post('http://localhost:8080/chess-trainer/move', body, {
+    return http.post(`${environment.apiUrl}/move`, body, {
       responseType: 'json',
     });
   }
 
   static deleteMove(body: DeleteMoveBody, http: HttpClient): Observable<Object> {
-    return http.request('DELETE', 'http://localhost:8080/chess-trainer/move', {
+    return http.request('DELETE', `${environment.apiUrl}/move`, {
       responseType: 'json',
       body: body,
     });
