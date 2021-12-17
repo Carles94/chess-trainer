da6a23f160aed99ee64e497ad32c972dfbbf3b28
Carles Tutusaus <ctutum@protonmail.com>
2021-12-17 20:06:02 +0100

    Added a selection of line in the select opening screen

--- chess-trainer/src/app/common/component/select-opening/select-opening.component.ts
+++ chess-trainer/src/app/common/component/select-opening/select-opening.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BLACK, WHITE } from '../../model/constant/constant';
+import { Line } from '../../model/interface/line';
 import { Opening } from '../../model/interface/opening';
 
 @Component({
@@ -12,14 +13,22 @@ export class SelectOpeningComponent implements OnInit {
   public BLACK = BLACK;
 
   public openingList: Opening[] = [];
+  public lineList: Line[] = [];
 
   public selectedColor = '';
+  public selectedOpening = '';
 
   constructor() {
     this.openingList.push({
       uuid: '',
-      name: 'name',
-      lineList: [],
+      name: 'opening',
+      lineList: [
+        {
+          name: 'line',
+          positionList: [],
+          color: WHITE,
+        },
+      ],
       color: WHITE,
     });
   }
@@ -29,5 +38,17 @@ export class SelectOpeningComponent implements OnInit {
   handleSelectColor(event: any): void {
     console.log(event);
     this.selectedColor = event.option.value;
+    this.lineList = [];
+    this.selectedOpening = '';
+  }
+
+  handleSelectOpening(event: any): void {
+    console.log(event);
+    this.selectedOpening = event.option.value.name;
+    this.lineList = this.openingList[0].lineList;
+  }
+
+  handleSelectLine(event: any): void {
+    console.log(event);
   }
 }
