64c38334cc9a7de3c1f03fa82d6fa4540267e51d
Carles Tutusaus <ctutum@protonmail.com>
2021-12-23 20:04:45 +0100

    Fixed viewChild problems

--- chess-trainer/src/app/common/component/select-opening/select-opening.component.ts
+++ chess-trainer/src/app/common/component/select-opening/select-opening.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { BLACK, WHITE } from '../../model/constant/constant';
 import { Opening } from '../../model/interface/opening';
 import { ShowOpeningsComponent } from './show-openings/show-openings.component';
@@ -9,16 +9,18 @@ import { ShowOpeningsComponent } from './show-openings/show-openings.component';
   styleUrls: ['./select-opening.component.scss'],
 })
 export class SelectOpeningComponent implements OnInit {
-  @ViewChild('showWhiteOpenings', { static: false })
-  public showWhiteOpenings!: ShowOpeningsComponent;
-  @ViewChild('showWhiteOpenings', { static: false })
-  public showBlackOpenings!: ShowOpeningsComponent;
+  @ViewChildren('showWhiteOpenings')
+  public showWhiteOpeningsQueryList!: QueryList<ShowOpeningsComponent>;
+  @ViewChildren('showBlackOpenings')
+  public showBlackOpeningsQueryList!: QueryList<ShowOpeningsComponent>;
   public WHITE = WHITE;
   public BLACK = BLACK;
   public openingList: Opening[] = [];
   public whiteOpeningList: Opening[] = [];
   public blackOpeningList: Opening[] = [];
   public selectedColor = '';
+  private showWhiteOpenings!: ShowOpeningsComponent;
+  private showBlackOpenings!: ShowOpeningsComponent;
 
   constructor() {
     this.openingList.push(
@@ -54,9 +56,22 @@ export class SelectOpeningComponent implements OnInit {
     this.blackOpeningList = this.openingList.filter((opening) => opening.color === BLACK);
   }
 
+  public ngAfterViewInit(): void {
+    this.showWhiteOpeningsQueryList.changes.subscribe((comps: QueryList<ShowOpeningsComponent>) => {
+      this.showWhiteOpenings = comps.first;
+    });
+    this.showBlackOpeningsQueryList.changes.subscribe((comps: QueryList<ShowOpeningsComponent>) => {
+      this.showBlackOpenings = comps.first;
+    });
+  }
+
   handleSelectColor(event: any): void {
     this.selectedColor = event.option.value;
-    this.showBlackOpenings.resetSelection();
-    this.showWhiteOpenings.resetSelection();
+    if (this.showBlackOpenings) {
+      this.showBlackOpenings.resetSelection();
+    }
+    if (this.showWhiteOpenings) {
+      this.showWhiteOpenings.resetSelection();
+    }
   }
 }
