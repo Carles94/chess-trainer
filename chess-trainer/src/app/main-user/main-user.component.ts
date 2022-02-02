f240728c489e0f1a87f7aa58eee264610457f19d
Carles Tutusaus <ctutum@protonmail.com>
2022-02-02 22:24:47 +0100

    Added angular theme

--- chess-trainer/src/app/main-user/main-user.component.ts
+++ chess-trainer/src/app/main-user/main-user.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-user',
   templateUrl: './main-user.component.html',
-  styleUrls: ['./main-user.component.scss']
+  styleUrls: ['./main-user.component.scss', './main-user.theme.scss'],
 })
 export class MainUserComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
